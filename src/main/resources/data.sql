f50940063248e45c9244c6a628cd556468de48d7
Jussi Nupponen <jussi.nupponen@gofore.com>
2017-01-20 09:54:53 +0200

    Added on conflict statement.

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,1 +1,3 @@
-INSERT INTO location values ('a', 'b', 'c', 'd', '2016-10-11T12:45:45');
+INSERT INTO location values ('a', 'b', 'c', 'd', '2016-10-11T12:45:45')
+ON CONFLICT(id) DO UPDATE
+SET secret = 'secret';
